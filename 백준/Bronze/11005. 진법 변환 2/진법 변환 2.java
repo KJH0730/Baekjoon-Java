cc9aff209c432dc62818a44b2dbce5587ad41e2c
김정환 <56757920+KJH0730@users.noreply.github.com>
2023-12-27 10:35:40 +0900

    [Bronze I] Title: 진법 변환 2, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,24 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
 
-        StringTokenizer st = new StringTokenizer(str);
-
-        int n = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        
-        int[] arr = new int[50];
+        int[] arr = new int[31];
         int arrayIndex = 0;
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = -1;
-        }
 
         while (n > 0) {
             arr[arrayIndex++] = n % b;
