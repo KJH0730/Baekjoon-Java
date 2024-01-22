a775a58a5d37394279fea2f3561eee6e3278a915
김정환 <56757920+KJH0730@users.noreply.github.com>
2024-01-22 12:48:01 +0900

    [Bronze V] Title: N 찍기, Time: 1060 ms, Memory: 30048 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=n; i++) {
+            System.out.println(i);
+        }
+    }
+}
