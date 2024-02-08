a227974d60de6e2e2468c3cdfd0a9de9ca8f32b0
김정환 <56757920+KJH0730@users.noreply.github.com>
2024-02-09 00:21:37 +0900

    [Bronze I] Title: 더하기 사이클, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,30 +1,22 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.io.IOException;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int num = Integer.parseInt(br.readLine());
-        String temp = String.valueOf(num);
-
-        if(num > 10) {
-            temp = "0" + temp;
-        }
-
-        int cnt = 0;
-
-        while(true) {
-            cnt++;
-
-            int sum = (Integer.parseInt(temp)/10) + (Integer.parseInt(temp)%10);
-            temp = String.valueOf(Integer.parseInt(temp)%10) + sum % 10;
-
-            if(Integer.parseInt(temp) == num) {
-                System.out.println(cnt);
-                break;
-            }
-        }
-    }
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		int copy = N;
+        
+		do {
+			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			cnt++;
+		} while (copy != N);
+		
+		System.out.println(cnt);
+	}
 }
