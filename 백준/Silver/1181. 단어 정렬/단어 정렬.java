6e1b604e7578d520d491d6979746bac74fa515a7
김정환 <56757920+KJH0730@users.noreply.github.com>
2024-02-12 13:55:43 +0900

    [Silver V] Title: 단어 정렬, Time: 348 ms, Memory: 25824 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,39 +1,46 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        String[] arr = new String[n];
-
-        for(int i=0; i<n; i++) {
-            arr[i] = br.readLine();
-        }
-
-        Arrays.sort(arr, (str1, str2) -> {
-            if(str1.length() == str2.length()) {
-                for(int i=0; i<str1.length(); i++) {
-                    if(str1.charAt(i) != str2.charAt(i))
-                        return str1.charAt(i) - str2.charAt(i);
-                }
-                return 0;
-            } else {
-                return str1.length() - str2.length();
-            }
-        });
-
-        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>(Arrays.asList(arr));
-
-        String[] strArr = linkedHashSet.toArray(new String[] {});
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i< strArr.length; i++) {
-            sb.append(strArr[i]).append("\n");
-        }
-
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+ 
+		String[] arr = new String[N];
+ 
+		for (int i = 0; i < N; i++) {
+			arr[i] = br.readLine();
+		}
+		
+		Arrays.sort(arr, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				// 단어 길이가 같을 경우 
+				if (s1.length() == s2.length()) {
+					return s1.compareTo(s2);
+				} 
+				// 그 외의 경우 
+				else {
+					return s1.length() - s2.length();
+				}
+			}
+		});
+ 
+		StringBuilder sb = new StringBuilder();
+ 
+		sb.append(arr[0]).append('\n');
+		
+		for (int i = 1; i < N; i++) {
+			// 중복되지 않는 단어만 출력
+			if (!arr[i].equals(arr[i - 1])) {
+				sb.append(arr[i]).append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
+ 
 }
