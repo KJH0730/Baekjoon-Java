fc6f57b508280285ee56262d9a7cf97df0a966ab
김정환 <56757920+KJH0730@users.noreply.github.com>
2024-02-20 20:43:44 +0900

    [Silver V] Title: 숫자 카드, Time: 860 ms, Memory: 176268 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[20000001];
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            arr[Integer.parseInt(st.nextToken()) + 10000000]++;
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<m; i++) {
+            if(arr[Integer.parseInt(st.nextToken()) + 10000000] != 0) {
+                sb.append(1).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
+        }
+
+        sb.deleteCharAt(sb.length()-1);
+
+        System.out.println(sb);
+    }
+}
