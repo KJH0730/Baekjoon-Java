0bc87fcdc3d1081745e00ebec7197b86e04e8b76
김정환 <56757920+KJH0730@users.noreply.github.com>
2024-02-26 23:13:56 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,35 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-
+import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-
-        BigInteger value = new BigInteger("1");
-        if(n != 0) {
-            for(int i=n; i>0; i--) {
-                value = value.multiply(BigInteger.valueOf(i));
-            }
-
-            String strValue = String.valueOf(value);
-
-            int result = 0;
-            for(int i=strValue.length()-1; i >= 0; i--) {
-                if(strValue.charAt(i) == '0') {
-                    result++;
-                } else {
-                    break;
-                }
-            }
-
-            System.out.println(result);
-        } else {
-            System.out.println(0);
+        int count = 0;
+        while(n >= 5) {
+            count += n / 5;
+            n /= 5;
         }
-
+        System.out.println(count);
     }
 }
